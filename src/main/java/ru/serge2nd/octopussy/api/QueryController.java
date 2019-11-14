44f63a2b0eda6ee9db0a7a16bc991d80c12fedab
Sergey.Pistyulga <Sergey.Pistyulga@nexign-systems.com>
2019-11-14 19:16:46 +0300

    Fix returning CLOB

--- src/main/java/ru/serge2nd/octopussy/api/QueryController.java
+++ src/main/java/ru/serge2nd/octopussy/api/QueryController.java
@@ -1,7 +1,5 @@
 package ru.serge2nd.octopussy.api;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,28 +10,23 @@ import ru.serge2nd.octopussy.data.NativeQueryAdapter;
 import ru.serge2nd.octopussy.data.NativeQueryAdapterProvider;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
 import static java.util.Collections.singletonMap;
-import static java.util.Objects.requireNonNull;
 
 @Validated
 @RestController
 @RequiredArgsConstructor
 public class QueryController {
-    private static final TypeReference<?> RESULT_TARGET_TYPE = new TypeReference<ArrayList>() {};
-
     private final NativeQueryAdapterProvider queryAdapterFactory;
-    private final ObjectMapper objectMapper;
 
     @PostMapping("executeQuery")
     public @ResponseBody List<?> executeQuery(@Valid @RequestBody QueryRq rq) {
         NativeQueryAdapter queryAdapter = queryAdapterFactory.getQueryAdapter(rq.getEnvId());
-        List<?> result = queryAdapter.execute(rq.getQueries().get(0));
+        String query = rq.getQueries().get(0);
 
-        return objectMapper.convertValue(result, RESULT_TARGET_TYPE);
+        return queryAdapter.execute(query);
     }
 
     @PostMapping("executeUpdate")
--- src/main/java/ru/serge2nd/octopussy/data/NativeQueryAdapterImpl.java
+++ src/main/java/ru/serge2nd/octopussy/data/NativeQueryAdapterImpl.java
@@ -1,11 +1,17 @@
 package ru.serge2nd.octopussy.data;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+import java.sql.Clob;
+import java.sql.SQLException;
 import java.util.List;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
 
 @RequiredArgsConstructor
 public class NativeQueryAdapterImpl implements NativeQueryAdapter {
@@ -13,7 +19,10 @@ public class NativeQueryAdapterImpl implements NativeQueryAdapter {
 
     @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public List<?> execute(String query) {
-        return entityManager.createNativeQuery(query).getResultList();
+        List<?> resultList = entityManager.createNativeQuery(query).getResultList();
+        return resultList.stream()
+                .map(NativeQueryAdapterImpl::extractRow)
+                .collect(toList());
     }
 
     @Transactional
@@ -21,5 +30,28 @@ public class NativeQueryAdapterImpl implements NativeQueryAdapter {
         return queries.stream()
                 .mapToInt(query -> entityManager.createNativeQuery(query).executeUpdate())
                 .sum();
+    }
+
+    private static Object extractRow(Object row) {
+        if (row instanceof Object[]) {
+            return Stream.of((Object[])row)
+                    .map(NativeQueryAdapterImpl::extractValue)
+                    .collect(toList());
+        }
+
+        return extractValue(row);
+    }
+
+    private static Object extractValue(Object cell) {
+        if (!(cell instanceof Clob))
+            return cell;
+
+        Clob clob = (Clob) cell;
+
+        try {
+            return clob.getSubString(1, (int)clob.length());
+        } catch (SQLException e) {
+            throw new DataRetrievalFailureException("cannot obtain LOB", e);
+        }
     }
 }
