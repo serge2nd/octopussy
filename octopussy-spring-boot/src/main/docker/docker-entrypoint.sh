5db31b3db96364c6f6657af98f5a02882479ee1a
Sergey Pistyulga <serge2nd@yandex.ru>
2021-02-24 11:02:20 +0300

    Fix POM, Dockerfile

--- octopussy-spring-boot/src/main/docker/docker-entrypoint.sh
+++ octopussy-spring-boot/src/main/docker/docker-entrypoint.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 set -exf
 
-set -u && mkdir -p "$EXT_DIR" && set +u
+ext0_dir=/extra0
+ext1_dir=/extra1
+mkdir -p "$ext0_dir" "$ext1_dir"
 
 [ -n "$JDBC_DRIVER_URLS" ] && for url in $JDBC_DRIVER_URLS; do
-    filepath="${EXT_DIR%/}/${url##*/}"
+    filepath="$ext1_dir/${url##*/}"
     [ -s "$filepath" ] || (wget -O "$filepath" "$url" || rm -f "$filepath")
 done
 
 # shellcheck disable=SC2154
-java $JVM_OPTS -Dloader.path="$EXT_DIR" -Dloader.debug=true -jar "${dist.file}"
+java $JVM_OPTS -Dloader.path="$ext0_dir,$ext1_dir" -Dloader.debug=true -jar "${dist.file}"
